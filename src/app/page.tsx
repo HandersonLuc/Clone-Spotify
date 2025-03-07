cc8b16a5224ce34e59f0371ab57024b0fed52a13
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-03-07 01:15:27 -0300

    Feat: "First commit"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,102 @@
-import Image from "next/image";
+import { Foooter } from '@/components/Footer';
+import { Sidebar } from '@/components/Sidebar';
+import {Home as SomeIcon, Search,Library, HomeIcon, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic, Mic2, LayoutListIcon, LayoutList, Laptop2, Volume, Fullscreen, Maximize2} from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <Sidebar/>
+        <main className="flex-1 p-6">
+          <div className='flex items-center gap-4'>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft/>
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight/>
+            </button>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <h1 className='font-semibold text-3xl mt-4'>Good Afternoon</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-3'>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/avenged_sevenfold.jpg"  width={104} height={104} alt="capa de album"/>
+              <strong>Avenged Sevenfold</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/deep_house.jpeg"  width={104} height={104} alt="capa de album"/>
+              <strong>Deep House Chill 2025</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/from_zero.jpeg" width={104} height={104}  alt="capa de album"/>
+              <strong>From Zero</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/avenged_sevenfold.jpg" width= {104} height={104} alt="capa de album"/>
+              <strong>Avenged Sevenfold</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/avenged_sevenfold.jpg"  width={104} height={104} alt="capa de album"/>
+              <strong>Avenged Senvenfold</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 rounded-md flex items-center gap-4 overflow-hidden group hover:bg-white/10 transition-colors'> 
+              <Image src="/avenged_sevenfold.jpg"  width={104} height={104} alt="capa de album"/>
+              <strong>Avenged Senvenfold</strong>
+              <button className='invisible group-hover:visible w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-7'>Made for you</h2>
+
+          <div className='grid grid-cols-8 gap-4 mt-2'>
+            <a href='#' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <Image src="/avenged_sevenfold.jpg" className='w-full'  width={100} height={100} alt="capa de album"/>
+            <strong className='font-semibold'>Daily Mix</strong>
+            <span className='text-sm text-zinc-500'>Avenged Sevenfold</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <Image src="/avenged_sevenfold.jpg" className='w-full'  width={100} height={100} alt="capa de album"/>
+            <strong className='font-semibold'>Daily Mix</strong>
+            <span className='text-sm text-zinc-500'>Avenged Sevenfold</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <Image src="/avenged_sevenfold.jpg" className='w-full'  width={100} height={100} alt="capa de album"/>
+            <strong className='font-semibold'>Daily Mix</strong>
+            <span className='text-sm text-zinc-500'>Avenged Sevenfold</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <Image src="/avenged_sevenfold.jpg" className='w-full'  width={100} height={100} alt="capa de album"/>
+            <strong className='font-semibold'>Daily Mix</strong>
+            <span className='text-sm text-zinc-500'>Avenged Sevenfold</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <Image src="/avenged_sevenfold.jpg" className='w-full'  width={100} height={100} alt="capa de album"/>
+            <strong className='font-semibold'>Daily Mix</strong>
+            <span className='text-sm text-zinc-500'>Avenged Sevenfold</span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <Foooter/>
     </div>
   );
 }
